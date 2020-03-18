4ecab9835601b709c53fcfc9fd700bc51cb1b373
rmatsovgithub <61052591+rmatsovgithub@users.noreply.github.com>
2020-03-18 17:30:21 +0300

    26

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,13 +6,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Роман on 18.03.2020.
  */
-public class NavigationHelper {
+public class NavigationHelper extends HelperBase {
   FirefoxDriver wd;
 
   public NavigationHelper(FirefoxDriver wd){
-    this.wd = wd;
+    super(wd);
   }
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 }
