a91cc5a6b220b17b432834e4d19b67024756134b
rmatsovgithub <61052591+rmatsovgithub@users.noreply.github.com>
2020-03-18 16:28:42 +0300

    24
    application manager - delegation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/TestBase.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.addressbook.test;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+/**
+ * Created by Роман on 18.03.2020.
+ */
+public class TestBase {
+
+  protected final ApplicationManager app = new ApplicationManager();
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    app.stop();
+  }
+
+}
