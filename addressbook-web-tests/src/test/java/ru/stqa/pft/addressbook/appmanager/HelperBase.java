3a9c07a3b74b6416407798c274fa9b0db6433d4d
rmatsovgithub <61052591+rmatsovgithub@users.noreply.github.com>
2020-03-19 16:56:19 +0300

    33
    33. Еще одна оптимизация: если происходит модификация котаткта или
    группы, то в методе type можно реализовать проверку: если значение поля
    не будет изменяться, то можно оставить значение прежним и не тратить
    время на перезаполнение неизменных значений:

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -12,7 +12,7 @@ public class HelperBase {
   WebDriver wd;
 
   public HelperBase(WebDriver wd) {
-    this.wd=wd;
+    this.wd = wd;
   }
 
   protected void click(By locator) {
@@ -21,9 +21,12 @@ public class HelperBase {
 
   protected void type(By locator, String text) {
     click(locator);
-    if(text != null){
-      wd.findElement(locator).clear();
-      wd.findElement(locator).sendKeys(text);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (!existingText.equals(text)) {
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
     }
 
   }
@@ -36,8 +39,6 @@ public class HelperBase {
       return false;
     }
   }
-
-
 
 
 }
