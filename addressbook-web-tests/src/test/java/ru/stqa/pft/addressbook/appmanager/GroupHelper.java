c13805b3911b2b80ef306a4d1dca09aab209200b
rmatsovgithub <61052591+rmatsovgithub@users.noreply.github.com>
2020-03-20 21:51:03 +0300

    43
    43 переделывается метод testGroupDeletion(). Теперь помимо проверки двух
    списков типа groupdata по размеру добавляется сопоставление элементов
    каждого из списков before и after. Для этого из списка before удаляется
    по индексу один элемент который в ходе теста был удален. Затем в классе
    groupdate путем использования средств встроенных в idea добавляются
    методы tostring и equals и hashcode. А затем в классе теста в метод
    удаления добавляется еще одна проверка assertEquals в которую предаются
    два списка before и after.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -71,11 +71,10 @@ public class GroupHelper extends HelperBase {
 
   public List<GroupData> getGroupList() {
     List<GroupData> groups = new ArrayList<GroupData>();
-    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    List<WebElement>elements = wd.findElements(By.cssSelector("span.group"));
     for(WebElement element : elements){
       String name = element.getText();
-      GroupData group = new GroupData(name, null, null);
-      groups.add(group);
+      groups.add(new GroupData(name, null, null));
     }
     return groups;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,6 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
-
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", null,null));
     }
