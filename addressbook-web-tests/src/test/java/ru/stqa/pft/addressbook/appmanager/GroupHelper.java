c76c7ae94032d1f9cfb81f9872ce3bb30666b9ba
rmatsovgithub <61052591+rmatsovgithub@users.noreply.github.com>
2020-03-20 16:34:35 +0300

    42
    42 получение списка элементов станицы, перебор их, извлечение данных,
    создание собственным объектов для последующего сравнения.  В врезультате
    сравнение before и after будет пока только по количеству данных.В классе
    создания групп переменные before и after меняются на тип лист и
    инициализируются вызовом нового метода getGroupList(): в котором в итоге
    будет выдаваться arraylist заполненный объектами groupdata^ создается
    список webelements по cssSelector у ‘span.group”, затем каждый элемент
    участвует в создании объекта groupdata с только заполенным полем name и
    остальными полями null и потом каждый объект помещается в итоговый
    результат.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -3,7 +3,11 @@ package ru.stqa.pft.addressbook.appmanager;
 import model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Роман on 18.03.2020.
@@ -63,5 +67,16 @@ public class GroupHelper extends HelperBase {
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for(WebElement element : elements){
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
   }
 }
